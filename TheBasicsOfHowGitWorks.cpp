7fae4291c678c2d602c2faf8ccbcabc9c09ef166
sergej0071 <sergejdenisenko007@gmail.com>
2021-12-01 00:18:35 +0200

    Initial comit.
    Add gitignore and file cpp with tasks and basical architecture.

---
+++ TheBasicsOfHowGitWorks.cpp
@@ -0,0 +1,20 @@
+// Serhii Lipihin 1 
+// create Base basic multi-tier project architecture with 3 tiers.(DAL,BLL,WEB).In the DAL level, 
+// implement work with the MSSQLserver database using the framework EntityFramework(Core).
+// Create 2 models and implement interaction with the database using the repository pattern.
+// Also implement UnitOfWork patterns. 
+// Model Game: 
+	//Property name Type Comment
+	//Key String unique; (alias for link crating)
+	//Name string
+	//Description String
+	//Author id 
+	//
+	// Model Author: 
+	// Id Guid 
+	// Name string
+	// 
+// Serhii Lipihin 1 
+// Implement a layer of business logic. Use the service pattern.
+// Implement elementary Crud operations for work with models. Also need to use auto-mapper and pagination.
+// 
